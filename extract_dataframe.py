fbf8ef771929c15faa92f3785968411c12975e5c
Biruk Getaneh <bkgetmom@gmail.com>
2022-04-26 13:56:40 +0000

    Fix find_freinds_count Implementation

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -76,7 +76,9 @@ class TweetDfExtractor:
         return followers_count
 
     def find_friends_count(self)->list:
-        friends_count = 
+        friends_count = [x['user']['friends_count']for x in self.tweets_list]
+
+        return friends_count
 
     def is_sensitive(self)->list:
         try:
