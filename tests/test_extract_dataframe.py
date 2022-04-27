898ddd283a0111554423f3c6d42c4eacc8177b14
Biruk Getaneh <bkgetmom@gmail.com>
2022-04-28 02:31:48 +0300

    Fix test_find_followers_count

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -57,7 +57,7 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(self.df.find_screen_name(), name)
 
     def test_find_followers_count(self):
-        f_count = [551, 66, 1195, 2666, 28250]
+        f_count = [3, 3, 3, 3, 3]
         self.assertEqual(self.df.find_followers_count(), f_count)
 
     def test_find_friends_count(self):
